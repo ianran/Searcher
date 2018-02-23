4b8276d6ea6a482631c5e04d8e0a3f4199e2e69b
Ian Rankin <ianran@nmsu.edu>
2018-02-22 22:53:05 -0700

    Think I got labeling tool working

--- labelingTool/labelToolTest.py
+++ labelingTool/labelToolTest.py
@@ -12,23 +12,45 @@ import getch
 import glob
 import os
 
+
 # read in all png files
 imageFiles = glob.glob('*.png')
 
-print('f - people, j - no people')
+print('f - people, j - no people, b - back')
 
-# go through every file and classify image
-for file in imageFiles:
+lastFile = None
+lastAnswer = None
+
+def labelSingleImage(file):
+    global lastFile
+    global lastAnswer
+
     image = im.imread(file)
     plt.imshow(image)
     plt.pause(0.05)
+    lastFile = file
 
     # read in input to move file into correct folder.
     while True:
         x = getch.getch()
         if x == 'f':
             os.rename(file, 'people/' + file)
+            lastAnswer = 'people/'
             break
         elif x == 'j':
             os.rename(file, 'noPeople/' + file)
+            lastAnswer = 'noPeople/'
             break
+        elif x == 'b':
+            if lastFile == None:
+                print('Can not go back more than 1')
+            else:
+                os.rename(lastAnswer + lastFile, lastFile)
+                lastAnswer == None
+                tempLastFile = lastFile
+                lastFile == None
+                labelSingleImage(tempLastFile)
+
+# go through every file and classify image
+for file in imageFiles:
+    labelSingleImage(file)
