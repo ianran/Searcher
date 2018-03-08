c5e0d45f2496ad8a980de23abff3999bbcbb4504
Joellen Lansford <jojolansford@yahoo.com>
2018-03-08 15:20:32 -0700

    Added another optical flow example from opencv and added a check for empty frames to the lucas-kanade_opticalFlowExample

--- opticalFlow/lucas-kanade_opticalFlowExample.py
+++ opticalFlow/lucas-kanade_opticalFlowExample.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('DJI_0040.MOV')
+cap = cv2.VideoCapture('DJI_0043.MOV')
 
 # params for ShiTomasi corner detection
 feature_params = dict( maxCorners = 100,
@@ -25,32 +25,39 @@ p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 # Create a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
 
-while(1):
+while cap.isOpened():
+
     ret,frame = cap.read()
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # calculate optical flow
-    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+    if ret:
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Select good points
-    good_new = p1[st==1]
-    good_old = p0[st==1]
+        # calculate optical flow
+        p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 
-    # draw the tracks
-    for i,(new,old) in enumerate(zip(good_new,good_old)):
-        a,b = new.ravel()
-        c,d = old.ravel()
-        mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-        frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
-    img = cv2.add(frame,mask)
+        # Select good points
+        good_new = p1[st==1]
+        good_old = p0[st==1]
 
-    cv2.imshow('frame',img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
+        # draw the tracks
+        for i,(new,old) in enumerate(zip(good_new,good_old)):
+	    a,b = new.ravel()
+	    c,d = old.ravel()
+	    mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
+	    frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
 
-    # Now update the previous frame and previous points
-    old_gray = frame_gray.copy()
+        img = cv2.add(frame,mask)
+
+        if img.shape is None:
+            continue
+
+        cv2.imshow('frame',img)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+	    break
+
+        # Now update the previous frame and previous points
+        old_gray = frame_gray.copy()
     p0 = good_new.reshape(-1,1,2)
 
 cv2.destroyAllWindows()
