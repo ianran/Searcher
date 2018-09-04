851f1fd83c42ef9d58459069b5d64d827fd9c533
Ian Rankin <ianran@nmsu.edu>
2018-09-03 22:31:26 -0600

    Created script to process image files

---
+++ labelingTool/createZip.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name trad_training
+#SBATCH --output ~/trad_training.out
+#SBATCH --partition gpu
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 16
+#SBATCH --time 0-00:15:00
+#SBATCH --mail-user ianran@nmsu.edu
+##SBATCH --mail-type BEGIN
+##SBATCH --mail-type END
+##SBATCH --mail-type FAIL
+
+# Written Ian Rankin September 2018
+# Slurm script for running the training onto.
+
+
+# load needed modules
+module load python/351
+#module load tensorflow-dev/1.8.0
+
+# start running program
+python ~/Searcher/labelingTool/FeedImagesToNumpyZip.py ~/feed/ ~/Searcher/labelingTool/labels.csv 1
