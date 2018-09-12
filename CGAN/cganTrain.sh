549d11c41f6191e26891f1c80d7a2a42b333687f
Ian Rankin <ianran@nmsu.edu>
2018-09-12 14:45:26 -0600

    Added SLURM script

---
+++ CGAN/cganTrain.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name cgan1
+#SBATCH --output cgan1.out
+#SBATCH --partition gpu
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 24
+#SBATCH --time 2-00:00:00
+#SBATCH --mail-user ianran@nmsu.edu
+#SBATCH --mail-type BEGIN
+#SBATCH --mail-type END
+#SBATCH --mail-type FAIL
+#SBATCH --get-user-env
+
+# Written Ian Rankin September 2018
+# Slurm script for running the training onto.
+
+echo "Loading modules"
+# load needed modules
+module load python/351
+module load tensorflow-dev/1.8.0
+
+echo "Starting execution of program."
+# start running program
+
+python -u ~/Searcher/CGAN/CGAN.py
+
+echo "Completed execution"
