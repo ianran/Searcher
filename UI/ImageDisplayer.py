3b419ffb9b9e0d4c4ecf234c75aeb28b592cec07
Ian Rankin <ianran@nmsu.edu>
2018-10-16 23:17:44 -0600

    Working Image Displayer in demo form

--- UI/ImageDisplayer.py
+++ UI/ImageDisplayer.py
@@ -10,7 +10,10 @@
 
 import threading
 import scipy.ndimage as im
-import matplotlib.pyplot as plt
+import glob
+#import matplotlib.pyplot as plt
+import tkinter as tk
+from PIL import ImageTk, Image
 import platform
 import time
 if platform.system() == 'Windows':
@@ -20,19 +23,45 @@ else:
 
 
 
+
 class ImageDisplayer:
     # run
     # threaded run function for showing images.
     def run(self):
+        root = tk.Tk()
+        image = Image.open(self.files[self.idx]) #im.imread(self.files[self.idx])
+        image.thumbnail(self.displaySize)
+        photo = ImageTk.PhotoImage(image)
+
+        #canv = tk.Canvas(root, width=80, height=80, bg='white')
+        #canv.create_image(80,80,image=imageLabel)
+        label = tk.Label(root, image=photo)
+        label.image = photo
+        #The Pack geometry manager packs widgets in rows or columns.
+        #label.pack(side = "bottom", fill = "both", expand = "yes")
+        label.pack()
+
+        root.update_idletasks()
+        root.update()
+        #root.mainloop()
+
         while True:
-            image = im.imread(self.files[self.idx])
-            plt.clf()
-            plt.axis('off')
-            plt.title(self.files[self.idx])
+            im = Image.open(self.files[self.idx]) #im.imread(self.files[self.idx])
+            im.thumbnail(self.displaySize)
+            photo = ImageTk.PhotoImage(im)
+            #plt.clf()
+            #plt.axis('off')
+            #plt.title(self.files[self.idx])
             print('trying to show image')
-            plt.imshow(image)
+            print(self.files[self.idx])
+            #plt.imshow(image)
+            #image.show()
+            label.configure(image=photo)
+            label.image=im
+            root.update_idletasks()
+            root.update()
             print('Should have shown image')
-            plt.pause(0.05)
+            #plt.pause(0.05)
 
             char = 'p'
             while not(char == 's' or char == 'w' or char == 'q'):
@@ -43,7 +72,7 @@ class ImageDisplayer:
                 if self.idx > 0:
                     self.idx -= 1
             elif char =='w':
-                if self.idx >= len(self.files):
+                if self.idx < len(self.files):
                     self.idx += 1
             else:
                 return
@@ -52,6 +81,7 @@ class ImageDisplayer:
         self.idx = 0
         self.files = []
         self.running = False
+        self.displaySize = (544,306)
 
         self.thread = threading.Thread(target=self.run)#, args=[self])
 
@@ -63,10 +93,12 @@ class ImageDisplayer:
             self.files.extend(newFiles)
             if not self.running:
                 self.thread.start()
+                #self.run()
 
 
 
 display = ImageDisplayer()
 time.sleep(1)
-display.addImgFiles(['../data/grace_hopper.jpg'])
+files = glob.glob('../labelingTool/feed/*.jpg')
+display.addImgFiles(files)
 time.sleep(40)
