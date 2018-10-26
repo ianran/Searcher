7e9869b53a12e26debc27675767ddd4b0bad1c09
Ian Rankin <ianran@joker.nmsu.edu>
2018-10-26 11:15:56 -0600

    Updated script files for joker

--- CNN/CGAN/cganTrain.sh
+++ CNN/CGAN/cganTrain.sh
@@ -23,6 +23,6 @@ module load tensorflow-dev/1.8.0
 echo "Starting execution of program."
 # start running program
 
-python -u ~/Searcher/CGAN/CGANRealData.py
+python -u ~/Searcher/CNN/CGAN/CGANRealData.py
 
 echo "Completed execution"
