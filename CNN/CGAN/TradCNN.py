cfa0b8526b7e37ee2b73d8abd773884f23101446
Ian Rankin <ianran@nmsu.edu>
2018-10-28 13:49:36 -0600

    Added confusion matrix

--- CNN/CGAN/TradCNN.py
+++ CNN/CGAN/TradCNN.py
@@ -87,11 +87,51 @@ def validate(labels, images, batchSize, sess):
    # extra data from max number of batches to finish off validation check
    if (extraData > 0):
       validFeed[x] = images[numValidBatches*numBatch:len(labels)]
-      validFeed[y] = labels[numBatch*numValidBatches:len(labels)]
+      validFeed[y] = labels[numValidBatches*numBatch:len(labels)]
 
       correctlyIdent += sess.run(correct, feed_dict=validFeed)
 
    return correctlyIdent / len(labels)
+
+
+# test network
+# confusion matrix
+# realImages -> discrimative network -> accuracy(with real labels)
+# @param labels - all of the labels to validate using
+# @param images - all of the images to validate using
+#
+# @return - accuracy of dataset.
+def testNetwork(labels, images, batchSize, sess):
+   validFeed = {trainPhase: False}
+   numValidBatches = len(labels) // batchSize
+   extraData = len(labels) % batchSize
+
+   predictedLabels = np.empty(len(labels))
+   for j in range(numValidBatches):
+      validFeed[x] = images[j*numBatch:(j+1)*numBatch]
+      validFeed[y] = labels[j*numBatch:(j+1)*numBatch]
+
+      predictedLabels[j*numBatch:(j+1)*numBatch] = sess.run(predictedClass, feed_dict=validFeed)
+
+   # extra data from max number of batches to finish off validation check
+   if (extraData > 0):
+      validFeed[x] = images[numValidBatches*numBatch:len(labels)]
+      validFeed[y] = labels[numValidBatches*numBatch:len(labels)]
+
+      predictedLabels[numValidBatches*numBatch:len(labels)] = sess.run(correct, feed_dict=validFeed)
+
+
+   labelsClass = np.argmax(labels, axis=1)
+   # create confusion matrix
+   confMat = np.zeros(labels.shape[1])
+   for i in range(len(labels)):
+       confMat[predictedClass[i],labelsClass[i]] += 1
+   acc = (confMat[0,0] + confMat[1,1]) / (confMat[0,0] + confMat[0,1] + confMat[1,0] + confMat[1,1])
+   print('0 = noPeople, 1 = people')
+   print('x : predicted class, y : actual class')
+   print(confMat)
+   print('accuracy = ' + str(acc))
+
 
 
 ###################### Training
@@ -106,11 +146,12 @@ for i in range(numEpochs):
    sess.run(trainStep, feed_dict=feed)
 
    ######### validate network and save model
-   if (i % 50 == 49 or i == (numEpochs - 1)):
-      print('Validation accuracy = ' + \
-           str(validate(validLabelsFull, validImagesFull, numBatch, sess)))
+   if (i % 50 == 0 or i == (numEpochs - 1)):
+      #print('Validation accuracy = ' + \
+      #    str(validate(validLabelsFull, validImagesFull, numBatch, sess)))
+      testNetwork(validLabelsFull, validImagesFull, numBatch, sess)
    if i % 100 == 99 or i == (numEpochs - 1):
-       saver.save(sess, '../../models/cnn4', global_step=i)
+       saver.save(sess, '../../models/cnn5', global_step=i)
 
 ####################### After training.
 
